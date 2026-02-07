06279e9f76de4fd7eafcbfb234bc4a15401e81ca
sicario.dll <92854310+sicariodll@users.noreply.github.com>
2026-02-07 21:26:43 +0200

    Memor
    Signed-off-by: sicario.dll <92854310+sicariodll@users.noreply.github.com>

--- audiostream.h
+++ audiostream.h
@@ -1,30 +1,28 @@
-
 #pragma once
 
 class CAudioStream
 {
 public:
-	CAudioStream();
-	~CAudioStream();
+    CAudioStream();
+    ~CAudioStream();
 
-	void Play(char* szURL, float fX, float fY, float fZ, float fDist);
-	void Stop();
-
-	void Process();
+    void Play(char* szURL, float fX, float fY, float fZ, float fDist);
+    void Stop();
+    void Process();
 
 private:
-	static void CALLBACK AudioStreamMetaSync(HSYNC handle, DWORD channel, DWORD data, void* user);
-	void InitBass();
+    static void CALLBACK AudioStreamMetaSync(HSYNC handle, DWORD channel, DWORD data, void* user);
+    void InitBass();
 
-	HSTREAM m_hStream;
-	ULONGLONG m_ullLastUpdate;
-	float m_fX, m_fY, m_fZ;
-	float m_fDistance;
-	char* m_szTitle;
-	char* m_szLabel;
+    HSTREAM m_hStream;
+    ULONGLONG m_ullLastUpdate;
+    float m_fX, m_fY, m_fZ;
+    float m_fDistance;
+    char* m_szTitle;
+    char* m_szLabel;
 
-	struct {
-		unsigned char bInited : 1;
-		unsigned char bPlaying : 1;
-	} m_Flags;
+    struct {
+        unsigned char bInited : 1;
+        unsigned char bPlaying : 1;
+    } m_Flags;
 };
