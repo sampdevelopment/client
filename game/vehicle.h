1a016691801583f2ea950a8c10c1c3b35055d7b1
sicario.dll <92854310+sicariodll@users.noreply.github.com>
2026-02-08 01:07:41 +0200

    Bug fixes, improvements and refactoring.
    Signed-off-by: sicario.dll <92854310+sicariodll@users.noreply.github.com>

--- game/vehicle.h
+++ game/vehicle.h
@@ -1,129 +1,119 @@
-//----------------------------------------------------------
-//
-// SA:MP Multiplayer Modification For GTA:SA
-// Copyright 2004-2005 SA:MP team
-//
-// Version: $Id: vehicle.h,v 1.12 2006/03/20 17:44:20 kyeman Exp $
-//
-//----------------------------------------------------------
-
 #pragma once
 
 #include "game.h"
 #include "entity.h"
 
-//-----------------------------------------------------------
-
 enum eLandingGearState 
 {
-	LGS_CHANGING = 0,
-	LGS_UP = 1,
-	LGS_DOWN = 2,
+    LGS_CHANGING = 0,
+    LGS_UP       = 1,
+    LGS_DOWN     = 2,
 };
 
 class CVehicle : public CEntity
 {
 public:
+    CVehicle(int iType, float fPosX, float fPosY, float fPosZ, float fRotation = 0.0f, PCHAR szNumberPlate = nullptr);
+    virtual ~CVehicle();
 
-	CVehicle( int iType, float fPosX, float fPosY, float fPosZ, float fRotation = 0.0f, PCHAR szNumberPlate = NULL);
-	virtual ~CVehicle();
+    virtual void Add();
+    virtual void Remove();
 
-	virtual void Add();
-	virtual void Remove();
+    void ResetPointers();
+    void ProcessMarkers();
 
-	void  ResetPointers();
+    void SetLockedState(int iLocked);
+    UINT GetVehicleSubtype();
 
-	void  ProcessMarkers();
-	int	  m_dwMarkerID;
+    float GetHealth() const;
+    void SetHealth(float fHealth);
+    void SetColor(int iColor1, int iColor2);
 
-	void  SetLockedState(int iLocked);
-	UINT  GetVehicleSubtype();
+    BOOL HasSunk() const;
+    BOOL IsWrecked() const;
+    BOOL IsDriverLocalPlayer() const;
+    bool IsVehicleMatchesPedVehicle() const;
+    BOOL IsATrainPart() const;
+    BOOL HasTurret() const;
 
-	float GetHealth();
-	void  SetHealth(float fHealth);
-	void  SetColor(int iColor1, int iColor2);
+    void SetHydraThrusters(DWORD dwDirection);
+    DWORD GetHydraThrusters() const;
 
-	BOOL  HasSunk();
-	BOOL  IsWrecked();
-	BOOL  IsDriverLocalPlayer();
-	bool  IsVehicleMatchesPedVehicle();
-	BOOL  IsATrainPart();
-	BOOL  HasTurret();
+    void SetTankRot(float X, float Y);
+    float GetTankRotX() const;
+    float GetTankRotY() const;
 
-	void  SetHydraThrusters(DWORD dwDirection);
-	DWORD GetHydraThrusters();
+    float GetTrainSpeed() const;
+    void SetTrainSpeed(float fSpeed);
 
-	void  SetTankRot(float X, float Y);
-	float GetTankRotX();
-	float GetTankRotY();
+    void Explode();
+    void Fix();
+    void ClearLastWeaponDamage();
+    UINT GetPassengersMax() const;
 
-	float GetTrainSpeed();
-	void  SetTrainSpeed(float fSpeed);
+    void SetSirenOn(BOOL state);
+    BOOL IsSirenOn() const;
 
-	void  Explode();
-	void  Fix();
-	void  ClearLastWeaponDamage();
-	UINT  GetPassengersMax();
+    void SetLandingGearState(eLandingGearState state);
+    eLandingGearState GetLandingGearState() const;
+    bool IsLandingGearNotUp() const;
+    void SetLandingGearState(bool bUpState);
 
-	void    SetSirenOn(BOOL state);
-	BOOL    IsSirenOn();
-	void    SetLandingGearState(eLandingGearState state);
-	eLandingGearState	GetLandingGearState();
-	bool	IsLandingGearNotUp();
-	void	SetLandingGearState(bool bUpState);
-	void	SetInvulnerable(BOOL bInv);
-	BOOL	IsInvulnerable() { return m_bIsInvulnerable; };
-	void    SetEngineState(BOOL bState);
-	void	SetDoorState(int iState);
-	void	LinkToInterior(int iInterior);
-	bool	IsPrimaryPedInVehicle();
-	void	SetWheelPopped(DWORD wheelid, DWORD popped);
-	BYTE	GetWheelPopped(DWORD wheelid);
-	void	AttachTrailer();
-	void	DetachTrailer();
-	void    SetTrailer(CVehicle *pTrailer);
-	CVehicle* GetTrailer();
-	BOOL	IsRCVehicle();
+    void SetInvulnerable(BOOL bInv);
+    inline BOOL IsInvulnerable() const { return m_bIsInvulnerable; }
 
-	void UpdateDamage(int iPanels, int iDoors, unsigned char ucLights);
-	int GetCarPanelsDamageStatus();
-	int GetCarDoorsDamageStatus();
-	unsigned char GetCarLightsDamageStatus();
+    void SetEngineState(BOOL bState);
+    void SetDoorState(int iState);
+    void LinkToInterior(int iInterior);
+    bool IsPrimaryPedInVehicle() const;
 
-	void SetCarOrBikeWheelStatus(unsigned char ucStatus);
-	unsigned char GetCarOrBikeWheelStatus();
+    void SetWheelPopped(DWORD wheelid, DWORD popped);
+    BYTE GetWheelPopped(DWORD wheelid) const;
 
-	BOOL	IsOccupied();
-	void    Recreate();
-	BOOL	UpdateLastDrivenTime();
-	void	ProcessEngineAudio(BYTE byteDriverID);
-	void    RemoveEveryoneFromVehicle();
-	void	SetHornState(BYTE byteState);
-	BOOL	HasADriver();
-	BOOL	VerifyInstance();
+    void AttachTrailer();
+    void DetachTrailer();
+    void SetTrailer(CVehicle* pTrailer);
+    CVehicle* GetTrailer();
 
-	void ToggleWindow(unsigned char ucDoorId, bool bClosed);
-	void ToggleTaxiLight(bool bToggle);
-	void ToggleEngine(bool bToggle);
-	bool IsUpsideDown();
-	bool IsOnItsSide();
-	void SetLightState(BOOL bState);
-	void ToggleComponent(DWORD dwComp, FLOAT fAngle);
-	void SetFeature(bool bToggle);
-	void SetVisibility(bool bVisible);
-	void ToggleDoor(int iDoor, int iNodeIndex, float fAngle);
+    BOOL IsRCVehicle() const;
 
-	unsigned char GetNumOfPassengerSeats();
+    void UpdateDamage(int iPanels, int iDoors, unsigned char ucLights);
+    int GetCarPanelsDamageStatus() const;
+    int GetCarDoorsDamageStatus() const;
+    unsigned char GetCarLightsDamageStatus() const;
 
-	VEHICLE_TYPE	*m_pVehicle;
-	BOOL		m_bIsInvulnerable;
-	//BOOL		m_bIsLocked; // No control state
-	BOOL		m_bDoorsLocked; // Vehicle is enterable TRUE/FALSE
-	BYTE		m_byteObjectiveVehicle; // Is this a special objective vehicle? 0/1
-	BOOL		m_bSpecialMarkerEnabled;
-	DWORD		m_dwTimeSinceLastDriven;
-	BOOL		m_bHasBeenDriven;
-	CVehicle*   m_pTrailer;
+    void SetCarOrBikeWheelStatus(unsigned char ucStatus);
+    unsigned char GetCarOrBikeWheelStatus() const;
+
+    BOOL IsOccupied() const;
+    void Recreate();
+    BOOL UpdateLastDrivenTime();
+    void ProcessEngineAudio(BYTE byteDriverID);
+    void RemoveEveryoneFromVehicle();
+    void SetHornState(BYTE byteState);
+    BOOL HasADriver() const;
+    BOOL VerifyInstance() const;
+
+    void ToggleWindow(unsigned char ucDoorId, bool bClosed);
+    void ToggleTaxiLight(bool bToggle);
+    void ToggleEngine(bool bToggle);
+    bool IsUpsideDown() const;
+    bool IsOnItsSide() const;
+    void SetLightState(BOOL bState);
+    void ToggleComponent(DWORD dwComp, float fAngle);
+    void SetFeature(bool bToggle);
+    void SetVisibility(bool bVisible);
+    void ToggleDoor(int iDoor, int iNodeIndex, float fAngle);
+
+    unsigned char GetNumOfPassengerSeats() const;
+
+    VEHICLE_TYPE* m_pVehicle = nullptr;
+    BOOL m_bIsInvulnerable = FALSE;
+    BOOL m_bDoorsLocked = FALSE;
+    BYTE m_byteObjectiveVehicle = 0;
+    BOOL m_bSpecialMarkerEnabled = FALSE;
+    DWORD m_dwTimeSinceLastDriven = 0;
+    BOOL m_bHasBeenDriven = FALSE;
+    CVehicle* m_pTrailer = nullptr;
+    int m_dwMarkerID = 0;
 };
-
-//-----------------------------------------------------------
