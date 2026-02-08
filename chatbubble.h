3f38cb5a5f2938f06cc6e0d19765fd2e7ff69077
sicario.dll <92854310+sicariodll@users.noreply.github.com>
2026-02-08 12:45:06 +0200

    Bug fixes, improvements and refactoring.
    Signed-off-by: sicario.dll <92854310+sicariodll@users.noreply.github.com>

--- chatbubble.h
+++ chatbubble.h
@@ -1,24 +1,40 @@
-
 #pragma once
 
-typedef struct
+struct CHAT_BUBBLE_ENTRY
 {
-	bool bValid;
-	char szText[MAX_CHAT_BUBBLE_TEXT+1];
-	int iLineCount;
-	DWORD dwExpireTick;
-	float fDistance;
-	DWORD dwColor;
-} CHAT_BUBBLE_ENTRY;
+    char  szText[MAX_CHAT_BUBBLE_TEXT + 1];
+    DWORD dwColor;
+    DWORD dwExpireTick;
+    float fDistance;
+    int   iLineCount;
+    bool  bValid;
+
+    inline void Reset()
+    {
+        szText[0]   = '\0';
+        dwColor     = 0;
+        dwExpireTick = 0;
+        fDistance   = 0.0f;
+        iLineCount  = 0;
+        bValid      = false;
+    }
+};
 
 class CChatBubble
 {
 private:
-	CHAT_BUBBLE_ENTRY m_ChatBubbleEntries[MAX_PLAYERS];
+    CHAT_BUBBLE_ENTRY m_ChatBubbleEntries[MAX_PLAYERS];
 
 public:
-	CChatBubble();
+    CChatBubble();
 
-	void AddText(WORD wPlayerID, char* szText, DWORD dwColor, float fDistance, int iDuration);
-	void Draw();
+    void AddText(
+        WORD        wPlayerID,
+        const char* szText,
+        DWORD       dwColor,
+        float       fDistance,
+        int         iDuration
+    );
+
+    void Draw();
 };
